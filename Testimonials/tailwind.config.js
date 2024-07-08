ac326f1531629bbbc522a672c25550a982c8674b
Jeremiah Ranen R <141173239+JeremiahRanen7@users.noreply.github.com>
2024-07-08 12:10:02 +0530

    Add files via upload

---
+++ Testimonials/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        sans:'Barlow Semi Condensed , sans-serif'
+      }
+    },
+  },
+  plugins: [],
+}
+
